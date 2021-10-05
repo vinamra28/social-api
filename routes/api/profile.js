168e358eff9be7bd11b9ff70f34278013b6662c3
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-10-05 11:28:24 +0530

    Adds routes based on company which returns user details
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -406,4 +406,25 @@ router.get('/github/:username', (req, res) => {
   }
 });
 
+// @route    GET api/profile/me/:company
+// @desc     Get current users profile based on company name
+// @access   Private
+router.get('/me/:company', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ company: req.params.company }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
